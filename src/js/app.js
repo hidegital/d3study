509e56704ce9a98e17f3d1e1648740cb975b01d4
山内英夫 <wiz_yamauchi@yamauchihideo-no-MacBook-Pro.local>
2015-11-04 20:45:07 +0900

    d3js nv.js

--- src/js/app.js
+++ src/js/app.js
@@ -1,10 +1,21 @@
-var ua           = require('./ua.js'),
-    resizeMargin = require('./resizeMargin.js'),
-    nav          = require('./nav.js')
+d3.json('data/data.json', function(data) {
 
-var userAgent = window.navigator.userAgent.toLowerCase();
+    nv.addGraph(function() {
+        var chart = nv.models.multiBarChart()
+            .x(function(d) { return d.label })
+            .y(function(d) { return d.value })
+            .margin({top: 30, right: 20, bottom: 50, left: 100})
+            .showControls(false);
+//                    .tooltips(true)
 
-console.log('ddd');
-console.log('ccc');
+
+        chart.yAxis.tickFormat(d3.format(',.2f'));
+
+        d3.select('#chart1 svg').datum(data).call(chart);
+
+        return chart;
+    });
+
+});
 
 
